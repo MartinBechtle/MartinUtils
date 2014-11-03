c0e84acfe08c21325af6b6df072e90064bc53908
Martin <Martin@Martin-Acer>
2014-11-03 22:46:13 +0100

    Avevo dimenticato un file

---
+++ src/martinutils/io/RemoveDuplicate.java
@@ -0,0 +1,32 @@
+package martinutils.io;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import martinutils.runtime.ExecUtil;
+
+/**
+ * Rimuove righe duplicate da un file di testo utf-8
+ * @author Martin
+ */
+public class RemoveDuplicate
+{
+	public static void main(String[] args) throws IOException
+	{
+		if (args.length == 0)
+			ExecUtil.die("Usage: RemoveDuplicate <input.txt> <output.txt>");
+		
+		List<String> lines = FileUtil.readUTF8Lines( new File(args[0]) );
+		Set<String> uniqueLines = new HashSet<>(lines);
+		
+		BufferedWriter br = FileUtil.getUTF8Writer( new File(args[1]) );
+		for (String str : uniqueLines)
+			br.append(str + "\n");
+		
+		br.close();
+	}
+}
