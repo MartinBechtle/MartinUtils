9c776e72061132bab361aa3e423e3cddc572b111
martin <martin@go-ictlabs.com>
2015-02-11 12:48:57 +0100

    Committato test per Angelo

--- test/martinutils/xml/XmlAsStrUtilityTest.java
+++ test/martinutils/xml/XmlAsStrUtilityTest.java
@@ -7,17 +7,89 @@ import org.junit.Test;
 public class XmlAsStrUtilityTest
 {
 	@Test
+	public void testConstructor()
+	{
+		try {
+			new XmlAsStrUtility(null);
+			fail("IllegalArgumentException should be thrown");
+		}
+		catch (IllegalArgumentException e) { }
+		
+		try {
+			new XmlAsStrUtility(null);
+			fail("IllegalArgumentException should be thrown");
+		}
+		catch (IllegalArgumentException e) { }
+	}
+	
+	String testXml = "<nodo id=\"v001\">contenuto</nodo><duplicato id=\"\"/><duplicato id=\"\">asd</duplicato>"
+					+ "<para id=\"001\">siamo <para id=\"002\"><bold>fighi</bold> sai?</para></para>";
+	
+	@Test
 	public void testGetElementAsStr() 
 	{
-		String textXml = "<nodo id=\"v001\">contenut</nodo>";
+		XmlAsStrUtility util = new XmlAsStrUtility(testXml);
 		
-		XmlAsStrUtility util = new XmlAsStrUtility()
+		try {
+			util.getElementAsStr(null, "id", "v001");
+			fail("IllegalArgumentException should be thrown");
+		}
+		catch (IllegalArgumentException e) { }
+		
+		try {
+			util.getElementAsStr("asd", null, "v001");
+			fail("IllegalArgumentException should be thrown");
+		}
+		catch (IllegalArgumentException e) { }
+		
+		try {
+			util.getElementAsStr("asd", "id", null);
+			fail("IllegalArgumentException should be thrown");
+		}
+		catch (IllegalArgumentException e) { }
+		
+		try {
+			util.getElementAsStr("", "id", "v001");
+			fail("IllegalArgumentException should be thrown");
+		}
+		catch (IllegalArgumentException e) { }
+		
+		try {
+			util.getElementAsStr("nodo", "", "v001");
+			fail("IllegalArgumentException should be thrown");
+		}
+		catch (IllegalArgumentException e) { }
+		
+		String result = util.getElementAsStr("nodo", "id", "v001");
+		assertEquals("<nodo id=\"v001\">contenuto</nodo>", result);
+		
+		result = util.getElementAsStr("nodooo", "id", "v001");
+		assertEquals("", result);
+		
+		result = util.getElementAsStr("nodo", "iddd", "v001");
+		assertEquals("", result);
+		
+		result = util.getElementAsStr("nodooo", "id", "01");
+		assertEquals("", result);
+		
+		result = util.getElementAsStr("nodooo", "id", "");
+		assertEquals("", result);
+		
+		result = util.getElementAsStr("nodooo", "id", "");
+		assertEquals("", result);
+		
+		result = util.getElementAsStr("para", "id", "001");
+		assertEquals("<para id=\"001\">siamo <para id=\"002\"><bold>fighi</bold> sai?</para></para>", result);
 	}
 
 	@Test
 	public void testGetElementContentAsStr()
 	{
-		fail("Not yet implemented");
+		// per ora test semplice semplice, poi si fa più avanzato
+		XmlAsStrUtility util = new XmlAsStrUtility(testXml);
+		
+		String result = util.getElementAsStr("para", "id", "001");
+		assertEquals("siamo <para id=\"002\"><bold>fighi</bold> sai?</para>", result);
 	}
 
 }
