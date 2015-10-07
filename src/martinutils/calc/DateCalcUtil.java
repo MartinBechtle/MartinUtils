8b517aa6a8267bad7c0cc742b7db545a9a5dd959
martin <martin@go-ictlabs.com>
2015-10-07 11:04:56 +0200

    New date utils

--- src/martinutils/calc/DateCalcUtil.java
+++ src/martinutils/calc/DateCalcUtil.java
@@ -4,7 +4,12 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class DateCalcUtil {
-
+	
+	/**
+	 * Duration of a day in milliseconds (useful to do calculations on long vars representing time)
+	 */
+	public static final int DAY_DURATION = 1000 * 60 * 60 * 24;
+	
 	/**
 	 * Get today's date at 00:00
 	 * @return
@@ -14,6 +19,25 @@ public class DateCalcUtil {
 		Calendar c = Calendar.getInstance();
 
 	    // set the calendar to start of today
+	    c.set(Calendar.HOUR_OF_DAY, 0);
+	    c.set(Calendar.MINUTE, 0);
+	    c.set(Calendar.SECOND, 0);
+	    c.set(Calendar.MILLISECOND, 0);
+
+	    Date today = c.getTime();
+	    return today;
+	}
+	
+	/**
+	 * Return a copy of the specified date, set to midnight of that day
+	 * @return
+	 */
+	public static Date getDateAtMidnight(Date date) {
+		
+		Calendar c = Calendar.getInstance();
+		c.setTime(date);
+
+	    // set the calendar to start of the date
 	    c.set(Calendar.HOUR_OF_DAY, 0);
 	    c.set(Calendar.MINUTE, 0);
 	    c.set(Calendar.SECOND, 0);
