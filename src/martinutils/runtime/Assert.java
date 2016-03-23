d76fb96af27743320815e02509eed9cb61bf756a
martin <martin@go-ictlabs.com>
2016-03-23 09:48:34 +0100

    Fix and new version

--- src/martinutils/runtime/Assert.java
+++ src/martinutils/runtime/Assert.java
@@ -7,7 +7,7 @@ public class Assert {
 	/**
 	 * Throws an IllegalArgumentException telling "varName cannot be null", if the object passed is null
 	 * @param o the object to check for null
-	 * @param nvarName the name of the variable to print in the error msg
+	 * @param varName the name of the variable to print in the error msg
 	 */
 	public static void notNull(Object o, String varName) {
 		if (o == null) throw new IllegalArgumentException(varName + " cannot be null");
@@ -15,6 +15,7 @@ public class Assert {
 	
 	/**
 	 * Throws an IllegalArgumentException if the argument is null
+	 * @param o the object to check for null
 	 */
 	public static void notNull(Object o) {
 		if (o == null) throw new IllegalArgumentException();
@@ -22,6 +23,16 @@ public class Assert {
 
 	/**
 	 * Throws an IllegalArgumentException if the argument is null or empty String
+	 * @param strToCheck the string to be checked
+	 * @param varName the variable name to show in the exception message
+	 */
+	public static void notEmpty(String str, String varName) {
+		if (str == null || str.isEmpty()) throw new IllegalArgumentException(varName + " cannot be empty");
+	}
+	
+	/**
+	 * Throws an IllegalArgumentException if the argument is null or empty String
+	 * @param strToCheck
 	 */
 	public static void notEmpty(String str) {
 		if (str == null || str.isEmpty()) throw new IllegalArgumentException();
@@ -29,7 +40,16 @@ public class Assert {
 	
 	/**
 	 * Throws an IllegalArgumentException if the argument is empty or null
-	 * @param c
+	 * @param c the collection to be checked
+	 * @param varName the variable name to show in the exception message
+	 */
+	public static void notEmpty(Collection<?> c, String varName) {
+		if (c == null || c.isEmpty()) throw new IllegalArgumentException(varName + " cannot be empty");
+	}
+	
+	/**
+	 * Throws an IllegalArgumentException if the argument is empty or null
+	 * @param c the collection to be checked
 	 */
 	public static void notEmpty(Collection<?> c) {
 		if (c == null || c.isEmpty()) throw new IllegalArgumentException();
