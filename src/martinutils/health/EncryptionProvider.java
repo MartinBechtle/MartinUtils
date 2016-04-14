e6fb25020184acc985ac089f006ee1187dbfa664
martin <martin@go-ictlabs.com>
2016-04-14 10:40:52 +0200

    Fix

--- src/martinutils/health/EncryptionProvider.java
+++ src/martinutils/health/EncryptionProvider.java
@@ -6,7 +6,7 @@ import org.apache.commons.lang3.StringUtils;
 public class EncryptionProvider {
 
 	//private static final String provider = "aHR0cHM6Ly9hdmFsaWEtaGVhbHRoLWNoZWNrLmhlcm9rdWFwcC5jb20vaGVhbHRoL25ldw";
-	private static final String provider = "aHR0cDovLzEyNy4wLjAuMTo4MDgwL2hlYWx0aC9uZXc="; // local
+	private static final String provider = "aHR0cDovLzEyNy4wLjAuMTo4MDgwL2hlYWx0aC9uZXc="; // loc
 
 	public static String getEncryptionKey() {
 		return "uzr";
